973be5db8e68a613fb792a200195fed63d095fbb
Márcio Pessoa <marcio.pessoa@sciemon.com>
2018-10-17 17:08:24 -0300

    Minor updates

--- RGB.h
+++ RGB.h
@@ -1,6 +1,6 @@
 /* RGB.h - Tri color LED Management Library
  * 
- * Author: Márcio Pessoa <marcio.pessoa@sciemon.com>
+ * Author: Márcio Pessoa <marcio.pessoa@gmail.com>
  * Contributors: none
  * 
  * Change log

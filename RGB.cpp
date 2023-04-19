738076be91ab21c4cd991536b3db2e72d2d16363
Márcio Pessoa <marcio.pessoa@gmail.com>
2023-04-18 23:44:09 -0300

    Refactor: Rename variables

--- RGB.cpp
+++ RGB.cpp
@@ -1,85 +1,84 @@
 /* RGB.cpp - Tri color LED Management Library
- * 
+ *
  */
 
-#include "Arduino.h"
 #include "RGB.h"
+#include "Arduino.h"
 
 /* RGB
- * 
+ *
  * Description
  *   Manage a tri color LED.
- * 
- *   RGB(byte Rpin, byte Gpin, byte Bpin)
- * 
+ *
+ *   RGB(unsigned char Rpin, unsigned char Gpin, unsigned char Bpin)
+ *
  * Parameters
  *   Rpin: Arduino red LED pin
  *   Gpin: Arduino green LED pin
  *   Bpin: Arduino blue LED pin
- * 
+ *
  * Returns
  *   void
  */
-RGB::RGB(byte Rpin, byte Gpin, byte Bpin) {
-  pinMode(Rpin, OUTPUT);
-  pinMode(Gpin, OUTPUT);
-  pinMode(Bpin, OUTPUT);
-  _Rpin = Rpin;
-  _Gpin = Gpin;
-  _Bpin = Bpin;
+RGB::RGB(unsigned char red_pin, unsigned char green_pin,
+         unsigned char blue_pin) {
+  pinMode(red_pin, OUTPUT);
+  pinMode(green_pin, OUTPUT);
+  pinMode(blue_pin, OUTPUT);
+  _red_pin = red_pin;
+  _green_pin = green_pin;
+  _blue_pin = blue_pin;
   _brightness = 1;
   _inverse = false;
 }
 
 /* inverse
- * 
+ *
  * Description
  *   Set LED color.
- * 
+ *
  *   a_led.inverse()
- * 
+ *
  * Parameters
  *   inverse: true of false
- * 
+ *
  * Returns
  *   void
  */
-void RGB::inverse() {
-  _inverse = !_inverse;
-}
+void RGB::inverse() { _inverse = !_inverse; }
 
 /* set
- * 
+ *
  * Description
  *   Set LED color.
- * 
- *   a_led.set(byte R, byte G, byte B)
- * 
+ *
+ *   a_led.set(unsigned char R, unsigned char G, unsigned char B)
+ *
  * Parameters
  *   Rvalue: Set red led intensity
  *   Gvalue: Set green led intensity
  *   Bvalue: Set blue led intensity
- * 
+ *
  * Returns
  *   void
  */
-void RGB::set(byte R, byte G, byte B) {
-  _R = R;
-  _G = G;
-  _B = B;
+void RGB::set(unsigned char red, unsigned char green, unsigned char blue) {
+  _red = red;
+  _green = green;
+  _blue = blue;
   refresh();
 }
 
 /* brightness
- * 
+ *
  * Description
  *   Set LED brightness.
- * 
+ *
  *   a_led.set(float brightness)
- * 
+ *
  * Parameters
  *   brightness: Brightness value (0 to 1)
- * 
+ *
  * Returns
  *   void
  */
@@ -89,33 +88,34 @@ void RGB::brightness(float brightness) {
 }
 
 /* refresh
- * 
+ *
  * Description
  *   Refresh LED.
- * 
+ *
  *   a_led.refresh()
- * 
+ *
  * Parameters
  *   none
- * 
+ *
  * Returns
  *   void
  */
 void RGB::refresh() {
-  byte R = _R * _brightness;
-  byte G = _G * _brightness;
-  byte B = _B * _brightness;
+  unsigned char red = _red * _brightness;
+  unsigned char green = _green * _brightness;
+  unsigned char blue = _blue * _brightness;
   if (_inverse) {
     //~ R = map(R, 0, 255, 255, 0);
     //~ G = map(G, 0, 255, 255, 0);
     //~ B = map(B, 0, 255, 255, 0);
-    R = 255 - R;
-    G = 255 - G;
-    B = 255 - B;
+    red = 255 - red;
+    green = 255 - green;
+    blue = 255 - blue;
   }
   // Debug information
-  //~ Serial.println(String(R) + "," + String(G) + "," + String(B) + "," + _brightness);
-  analogWrite(_Rpin, R);
-  analogWrite(_Gpin, G);
-  analogWrite(_Bpin, B);
+  //~ Serial.println(String(R) + "," + String(G) + "," + String(B) + "," +
+  //_brightness);
+  analogWrite(_red_pin, red);
+  analogWrite(_green_pin, green);
+  analogWrite(_blue_pin, blue);
 }
--- RGB.h
+++ RGB.h
@@ -1,18 +1,18 @@
 /* RGB.h - Tri color LED Management Library
- * 
+ *
  * Author: Márcio Pessoa <marcio.pessoa@gmail.com>
  * Contributors: none
- * 
+ *
  * Change log
  * 2015-09-04
  *         * refresh
  *         Optimized code on inverse method.
- * 
+ *
  * 2015-07-04
  *         * inverse
  *         Fixed bug on inverse method.
  *         Finnaly all is working fine.
- * 
+ *
  * 2015-07-03
  *         * refresh
  *         Added refresh private method to update LED after some operations.
@@ -20,16 +20,16 @@
  *         brightness method now refrsh LED status.
  *         * set
  *         set mothod uses refresh method to update LED status.
- * 
+ *
  * 2015-06-13
  *         * set
  *         Added bright variable to control led bright.
- * 
+ *
  * 2015-02-22
  *         * set
  *         Added set(unsigned long value) to use color name definitions and to
- *         overload set(byte R, byte G, byte B).
- * 
+ *         overload set(unsigned char R, unsigned char G, unsigned char B).
+ *
  * 2015-02-21
  *         Experimental version.
  */
@@ -39,23 +39,23 @@
 
 #include "Arduino.h"
 
-class RGB
-{
-  public:
-    RGB(byte Rpin, byte Gpin, byte Bpin);
-    void inverse();
-    void set(byte R, byte G, byte B);
-    void brightness(float brightness = 1);
-  private:
-    void refresh();
-    byte _Rpin;
-    byte _Gpin;
-    byte _Bpin;
-    byte _R;
-    byte _G;
-    byte _B;
-    float _brightness;
-    bool _inverse;
+class RGB {
+ public:
+  RGB(unsigned char red_pin, unsigned char green_pin, unsigned char blue_pin);
+  void inverse();
+  void set(unsigned char red, unsigned char green, unsigned char blue);
+  void brightness(float brightness = 1);
+
+ private:
+  void refresh();
+  unsigned char _red_pin;
+  unsigned char _green_pin;
+  unsigned char _blue_pin;
+  unsigned char _red;
+  unsigned char _green;
+  unsigned char _blue;
+  float _brightness;
+  bool _inverse;
 };
 
 #endif
